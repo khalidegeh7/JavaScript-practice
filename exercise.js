2b36139984bca10fd9d176c7a990173c1c030e21
khalidegeh7 <khalidegeh123@gmail.com>
2024-10-12 00:19:52 +0100

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,18 @@
+//declare and assign variable
+const isWaterWet = true;
+
+//declare variable and assign it later
+let isLoggedIn = null;
+console.log(isLoggedIn)
+isLoggedIn = true;
+
+let score = 1;
+score= score +1
+score= score -1
+score= score /2
+score= score **3
+score= score %2
+console.log(score)
+
+let name = "Alex"
+console.log(name)
